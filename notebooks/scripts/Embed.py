f639d6b26779ff13f5ebf5834096e9ba72733eee
Sravani Nanduri <sravani.nanduri.sn@gmail.com>
2020-08-13 17:35:53 -0700

    adding embed.py and some output (not all of it because of memory

--- notebooks/scripts/Embed.py
+++ notebooks/scripts/Embed.py
@@ -2,8 +2,10 @@ import argparse
 from augur.utils import write_json
 import Bio.SeqIO
 from collections import OrderedDict
+import pandas as pd
 from scipy.spatial.distance import squareform
-from sklearn.manifold import TSNE
+from sklearn.manifold import TSNE, MDS
+from umap import UMAP
 import sys
 
 from Helpers import get_hamming_distances
@@ -11,7 +13,7 @@ from Helpers import get_hamming_distances
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "creates embeddings", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     
-    subparsers = parser.add_subparsers()
+    subparsers = parser.add_subparsers(dest="command")
     
     tsne = subparsers.add_parser("t-sne")
     tsne.add_argument("--alignment", required=True, help="an aligned FASTA file from a multiple sequence alignment")
@@ -20,12 +22,28 @@ if __name__ == "__main__":
     tsne.add_argument("--output-node-data", help="outputting a node data JSON file")
     tsne.add_argument("--output-dataframe", help="outputting a csv file")
     
+    umap = subparsers.add_parser("umap")
+    umap.add_argument("--alignment", required=True, help="an aligned FASTA file from a multiple sequence alignment")
+    umap.add_argument("--nearest-neighbors", default=200, type=int, help="the nearest neighbors value for the umap embedding")
+    umap.add_argument("--min-dist", default=.5, type=float, help="the minimum distance value for the umap embedding")
+    umap.add_argument("--output-node-data", help="outputting a node data JSON file")
+    umap.add_argument("--output-dataframe", help="outputting a csv file")
+    
+    mds = subparsers.add_parser("mds")
+    mds.add_argument("--alignment", required=True, help="an aligned FASTA file from a multiple sequence alignment")
+    mds.add_argument("--components", default=10, type=int, help="the number of components for MDS")
+    mds.add_argument("--output-node-data", help="outputting a node data JSON file")
+    mds.add_argument("--output-dataframe", help="outputting a csv file")
+    
     args = parser.parse_args()
     
     if args.output_node_data is None and args.output_dataframe is None:
         print("You must specify one of the outputs", file=sys.stderr)
         sys.exit(1)
-    
+    options = ["pca", "mds", "tsne", "umap"]
+    if args.command not in options:
+        print("You must specify one of the options allowed:" + str(options[i] for i in options), file=sys.stderr)
+        sys.exit(1)
     # Load alignment
     sequences_by_name = OrderedDict()
 
@@ -41,15 +59,46 @@ if __name__ == "__main__":
     distance_matrix = squareform(hamming_distances)
     
     # Calculate Embedding
-    embedding_class = TSNE #conditionally learn name of subparser 
-    embedding_parameters = {
-        "metric": "precomputed",
-        "perplexity": args.perplexity,
-        "learning_rate": args.learning_rate
-    }
+    if args.command == "t-sne":
+        embedding_class = TSNE #conditionally learn name of subparser 
+        embedding_parameters = {
+            "metric": "precomputed",
+            "perplexity": args.perplexity,
+            "learning_rate": args.learning_rate
+        }
+    elif args.command == "umap":
+        embedding_class = UMAP
+        embedding_parameters = {
+            "n_neighbors": args.nearest_neighbors,
+            "min_dist": args.min_dist,
+            "n_components": 2,
+            "init": "spectral"
+        }
+    elif args.command == "mds":
+        embedding_class = MDS
+        embedding_parameters = {
+            "dissimilarity": "precomputed",
+            "n_components": args.components
+        }
+
     embedder = embedding_class(**embedding_parameters)
     embedding = embedder.fit_transform(distance_matrix)
     
     print(embedding)
+    
     # Output Embedding
         # create dictionary to be "wrapped" by write_json
+        
+    embedding_df = pd.DataFrame(embedding)
+    if args.command == "mds" or args.command == "pca":
+        embedding_df.columns=[args.command + str(i) for i in range(1,args.components + 1)]
+    else:
+        embedding_df.columns = [args.command.replace('-', '') + "_x" , args.command.replace('-', '') + "_y"]
+        
+    embedding_df.index = sequence_names
+    if args.output_node_data is not None:
+        embedding_dict = embedding_df.transpose().to_dict()
+        write_json(embedding_dict,"results/embed_" + args.command.replace('-', '') + ".json")
+    if args.output_dataframe is not None:
+        embedding_df.to_csv("results/embed_" + args.command.replace('-', '') + ".csv")
+    
