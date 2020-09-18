a00443ac94d663141ce777a7ea1d20045a1cf769
nandsra21 <sravani.nanduri@outlook.com>
2020-09-18 01:06:07 -0700

    adding slight fix to scatterplot code - issue with title string

--- notebooks/scripts/scatterplot.py
+++ notebooks/scripts/scatterplot.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
 
             ax.set_xlabel("Genetic distance")
             ax.set_ylabel(f"Euclidean distance ({args.method})")
-            ax.set_title(f"Euclidean distance ({args.method}) vs. genetic distance ($R^2={mean:.3f}$ +/- {std:.3f}$)")
+            ax.set_title(f"Euclidean distance ({args.method}) vs. genetic distance ($R^2={mean:.3f} +/- {std:.3f}$)")
 
             sns.despine()
             
