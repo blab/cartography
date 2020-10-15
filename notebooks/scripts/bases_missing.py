02052a81b2f895cc6eeee1d5c38bba59e204c0eb
nandsra21 <sravani.nanduri@outlook.com>
2020-10-14 22:38:42 -0700

    adding changes to Snakefiles, (dropped strains for mers, bootstrapping value for zika)

--- notebooks/scripts/bases_missing.py
+++ notebooks/scripts/bases_missing.py
@@ -1,5 +1,5 @@
 """
-Takes aligned strain data from snp sites and outputs a two dataframes: strains_df and genomes_df, which can be converted to lists for hamming distance
+Takes aligned strain data and outputs a two dataframes: strains_df and genomes_df, which can be converted to lists for hamming distance
 """
 import pandas as pd
 import numpy as np
@@ -9,58 +9,51 @@ import re
 from pathlib import Path
 import argparse
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--alignment", required=True, help="FASTA file of sequences")
+    parser.add_argument("--output-fasta", required=True, help="name of disease")
+        
+    args = parser.parse_args()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--sequences", required=True, help="FASTA file of sequences")
-parser.add_argument("--dropped_strains", required=True, help="strains to drop")
-parser.add_argument("--disease_name", required=True, help="name of disease")
-	
-args = parser.parse_args()
-
-f = open(args.disease_name, "r")
-virus_name = f.read()
-
-dropped_strains = [line.rstrip('\n') for line in open(args.dropped_strains,"r")]
-
-strains = []
-genomes = []
-for record in SeqIO.parse(sequences, "fasta"):
-    if(record.id not in dropped_strains):
+    strains = []
+    genomes = []
+    for record in SeqIO.parse(args.alignment, "fasta"):
         strains.append(str(record.id))
         genomes.append(str(record.seq))
-		
-genomes_df = pd.DataFrame(genomes)
-strains_df = pd.DataFrame(strains)
-strains_df.columns = ["strain"]
+            
+    genomes_df = pd.DataFrame(genomes)
+    strains_df = pd.DataFrame(strains)
+    strains_df.columns = ["strain"]
 
-#Checking missing_bases
+    #Checking missing_bases
 
-genomes_missing_bases = []
-for x in genomes:
-    x = re.sub(r'[^AGCT]', '5', x)
-    numberOfN = x.count("5")
-    genomes_missing_bases.append(numberOfN)
-	
-genomes_missing_bases_df = pd.DataFrame(genomes_missing_bases)
-genomes_missing_bases_df = genomes_missing_bases_df.merge(strains_df, how='outer', left_index = True, right_index = True)
-genomes_missing_bases_df.columns = ["bases missing", "strain"]
+    genomes_missing_bases = []
+    for x in genomes:
+        x = re.sub(r'[^AGCT]', '5', x)
+        numberOfN = x.count("5")
+        genomes_missing_bases.append(numberOfN)
+        
+    genomes_missing_bases_df = pd.DataFrame(genomes_missing_bases)
+    genomes_missing_bases_df = genomes_missing_bases_df.merge(strains_df, how='outer', left_index = True, right_index = True)
+    genomes_missing_bases_df.columns = ["bases_missing", "strain"]
 
-#outputs a dataframe that can be visualized in the jupyter notebook - a histogram detailing amount of missing bases.
+    threshold = np.mean(genomes_missing_bases) + 3*np.std(genomes_missing_bases)
+    genomes_missing_bases_df = genomes_missing_bases_df[genomes_missing_bases_df['bases_missing'] >= threshold]
+    list_of_val = genomes_missing_bases_df.index.tolist()
 
-genomes_missing_bases_df.to_csv('../Dataframes/missing_bases' + virus_name + '.csv',index = False)
+    strains = np.array(strains)
+    genomes = np.array(genomes)
 
-dropped_strains.extend(list(genomes_missing_bases_df[genomes_missing_bases_df["bases missing"]>1000]["strain"]))
-dropped_strains = pd.Series(dropped_strains) 
+    deleted_strains = list(np.delete(strains, list_of_val))
+    deleted_genomes = list(np.delete(genomes, list_of_val))
 
-# dropping all strains from the dataframes and lists that are in dropped_strains
+    if args.output_fasta is not None:
+        fasta_file = open(args.output_fasta, "w")
 
-indexNames = dropped_strains.isin(['strain']).index
+        for i in range(len(deleted_genomes)):
 
-strains_df.drop(indexNames , inplace=True)
-strains_df.reset_index(drop = True)
-strains_df.to_csv('../Dataframes/strains_dataframe' + virus_name + '.csv', index = False)
+            fasta_file.write(">" + deleted_strains[i] + "\n" + deleted_genomes[i] + "\n")
 
-genomes_df.drop(indexNames , inplace=True)
-genomes_df.columns = ["strain"]
-genomes_df.reset_index(drop = True)
-genomes_df.to_csv('../Dataframes/genomes_dataframe' + virus_name + '.csv', index = False)
+        fasta_file.close()
+
--- notebooks/scripts/concatenate_tables.py
+++ notebooks/scripts/concatenate_tables.py
@@ -18,4 +18,5 @@ if __name__ == "__main__":
         for table_file in args.tables
     ], ignore_index=True, sort=True)
 
+    df = df.sort_values(by=['matthews_cc'])
     df.to_csv(args.output, sep=args.separator, header=True, index=False)
