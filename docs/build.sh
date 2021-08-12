3f9c2aaa1f89642f328f564f3b6bfd35da395e65
John Huddleston <huddlej@gmail.com>
2021-08-12 13:58:10 -0700

    Stub out LaTeX version of the paper

---
+++ docs/build.sh
@@ -0,0 +1,15 @@
+TITLE=cartography
+
+rm -f *.log
+rm -f *.aux
+rm -f *.out
+rm -f *.bbl
+rm -f *.bcf
+rm -f *.blg
+rm -f .DS_Store
+rm -f ${TITLE}.pdf
+
+pdflatex -draftmode ${TITLE}
+bibtex ${TITLE}
+pdflatex -draftmode ${TITLE}
+pdflatex ${TITLE}
