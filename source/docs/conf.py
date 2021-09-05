2e70c8d2e27e30d28b3ba407a99d0406a8b86c0b
nandsra21 <sravani.nanduri@outlook.com>
2021-09-05 14:41:00 -0700

    adding long_description to source.py, the markdown sphinx documentation copied into README makes setup.py easier to maintain

--- source/docs/conf.py
+++ source/docs/conf.py
@@ -53,7 +53,7 @@ author = 'Sravani Nanduri and John Huddleston'#prose_list(git_authors())
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinxarg.ext', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinxarg.ext', 'sphinx.ext.napoleon', 'sphinx_markdown_builder']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
--- source/setup.py
+++ source/setup.py
@@ -1,8 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os 
+
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-
+    
 setup(
     name='embedGen',
     version='0.0.1',    
@@ -10,17 +12,22 @@ setup(
     url='https://github.com/blab/cartography/',
     author='Sravani Nanduri <nandsra@cs.washington.edu> , John Huddleston <huddlej@gmail.com>', 
     author_email='nandsra@cs.washington.edu',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license='MIT License',
-    packages=['seaborn', 'scikit-learn', 'umap-learn', 'matplotlib', 'pandas', 'numpy', 'hdbscan'],
-    install_requires=['augur>=12.0.0',
-                      'numpy', 
+    #project_urls = {
+    #    "Documentation": "" Do I need to publish my documentation somewhere in order to have this link work?
+    #}
+	packages=find_packages(exclude=['test']),
+    #packages=['seaborn', 'scikit-learn', 'umap-learn', 'matplotlib', 'pandas', 'numpy', 'hdbscan'],
+    install_requires=['numpy', 
                       'pandas'
                       ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
         'Intended Audience :: Science/Research',
-        'License :: MIT License',         
+        'License :: OSI Approved :: MIT License',         
         'Programming Language :: Python :: 3.7',
     ],
 )
