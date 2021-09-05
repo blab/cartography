8dfc311e5d873ddfc73f859cdfa6716e44f6b784
nandsra21 <sravani.nanduri@outlook.com>
2021-09-05 14:29:20 -0700

    Adding script that outputs node data without the help of the embed script

---
+++ notebooks/scripts/output_node_data.py
@@ -0,0 +1,19 @@
+"""Create node json (augur) from pandas dataframe.
+"""
+import argparse
+import pandas as pd
+from augur.utils import write_json
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--table", nargs="+", help="table to make node data from")
+    parser.add_argument("--separator", default=",", help="separator between columns in the given tables")
+    parser.add_argument("--node_name", default="nodes", help="what to name the node value in the auspice json")
+    parser.add_argument("--output", help="json file")
+
+    args = parser.parse_args()
+
+    
+    if args.output is not None:
+        embedding_dict = pd.DataFrame(args.table,sep=args.separator, index_col=0).transpose().to_dict()
+        write_json({args.node_name: embedding_dict}, args.output)
