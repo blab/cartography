2e02e11a250f6076a2442ec50992b947b8fcf8b6
John Huddleston <huddlej@gmail.com>
2021-11-01 10:08:54 -0700

    Fix bugs with output node data script
    Defines a conda environment for the "output node data" rule of the seasonal flu
    workflow and fixes bugs in the corresponding script including accepting only a
    single table input and reading that input with pandas `read_csv`.

--- notebooks/scripts/output_node_data.py
+++ notebooks/scripts/output_node_data.py
@@ -6,14 +6,17 @@ from augur.utils import write_json
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--table", nargs="+", help="table to make node data from")
+    parser.add_argument("--table", help="table to make node data from")
     parser.add_argument("--separator", default=",", help="separator between columns in the given tables")
     parser.add_argument("--node_name", default="nodes", help="what to name the node value in the auspice json")
     parser.add_argument("--output", help="json file")
 
     args = parser.parse_args()
 
-    
     if args.output is not None:
-        embedding_dict = pd.DataFrame(args.table,sep=args.separator, index_col=0).transpose().to_dict()
+        embedding_dict = pd.read_csv(
+            args.table,
+            sep=args.separator,
+            index_col=0
+        ).transpose().to_dict()
         write_json({args.node_name: embedding_dict}, args.output)
