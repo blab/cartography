81d0594d9a5b57ec485fd3031825a28b58f6c736
John Huddleston <huddlej@gmail.com>
2022-06-03 11:24:48 -0700

    Add generic script for plotting distances

--- notebooks/scripts/plot_distances.py
+++ notebooks/scripts/plot_distances.py
@@ -29,20 +29,33 @@ if __name__ == '__main__':
         index_col=0,
     )
 
-    assert np.array_equal(x.index.values, y.index.values)
+    assert np.array_equal(sorted(x.index.values), sorted(y.index.values))
+    x_indices_in_y = [np.where(y.index.values == x_value)[0][0] for x_value in x.index.values]
+    x_values = x.values
+    y_values = y.iloc[x_indices_in_y, x_indices_in_y].values
 
-    x_array = squareform(x.values)
-    y_array = squareform(y.values)
+    x_array = squareform(x_values)
+    y_array = squareform(y_values)
 
     regression = linregress(x_array, y_array)
     slope, intercept, r_value, p_value, std_err = regression
 
-    fig, ax = plt.subplots(1, 1, figsize=(6, 6), dpi=200)
-    ax.plot(
-        x_array,
-        y_array,
-        "o",
-        alpha=0.25,
+    fig, ax = plt.subplots(1, 1, figsize=(8, 8), dpi=200)
+    # ax.plot(
+    #     x_array,
+    #     y_array,
+    #     "o",
+    #     alpha=0.25,
+    # )
+    data = pd.DataFrame({"x": x_array, "y": y_array})
+    sns.boxplot(
+        x="x",
+        y="y",
+        data=data,
+        linewidth=0.75,
+        fliersize=0.1,
+        color="#CCCCCC",
+        ax=ax
     )
 
     ax.text(
