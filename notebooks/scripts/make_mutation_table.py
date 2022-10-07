1b4a6751428b6fa54df02f91f1f29448d645a4a7
John Huddleston <huddlej@gmail.com>
2022-10-07 15:01:36 -0700

    Ignore leading/trailing gaps and lack of consensus
    When building a mutation table for clusters, ignore leading and trailing
    gaps in the reference alignment and any characters representing lack of
    information (e.g., "X").

--- notebooks/scripts/make_mutation_table.py
+++ notebooks/scripts/make_mutation_table.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument("--alignment", required=True, help="the name of the aligned consensus file WITH reference")
+    parser.add_argument("--ignored-characters", nargs="+", default=["X"], help="list of characters to ignore in pairwise comparisons with the reference")
     parser.add_argument("--output", required=True, help="outputting a tsv file of mutations per cluster")
 
     args = parser.parse_args()
@@ -16,6 +17,24 @@ if __name__ == "__main__":
 
     # The reference is the first sequence.
     reference = next(sequences)
+
+    # Create a list of gap sites at the beginning and end of the reference
+    # sequence to ignore from the alignment. This happens when the reference is
+    # missing sequences that are present in the consensus sequences. These
+    # differences usually reflect missing data in the original sequencing of the
+    # reference and not a biologically-relevant insertion.
+    ignored_sites = []
+    site = 0
+    while reference.seq[site] == "-" and site < len(reference.seq):
+        ignored_sites.append(site)
+        site += 1
+
+    site = len(reference.seq) - 1
+    while reference.seq[site] == "-" and site >= 0:
+        ignored_sites.append(site)
+        site -= 1
+
+    print(f"Ignoring leading and trailing gaps in the reference at sites: {ignored_sites}")
 
     # The rest of the sequences are from the clusters.
     clusters = list(sequences)
@@ -27,7 +46,12 @@ if __name__ == "__main__":
             continue
 
         for site in range(len(reference.seq)):
-            if cluster.seq[site] != reference.seq[site]:
+            # Find mismatches of characters we don't ignore at sites we don't
+            # ignore.
+            if (site not in ignored_sites and
+                cluster.seq[site] != reference.seq[site] and
+                cluster.seq[site] not in args.ignored_characters and
+                reference.seq[site] not in args.ignored_characters):
                 # Report the site in 1-based coordinates.
                 mutations.append(
                     {
