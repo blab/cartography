92c405b5f7e2989abb4e417c49b2993d8104e061
nandsra21 <sravani.nanduri@outlook.com>
2022-11-20 15:42:22 -0800

    adding new mutation script and changes to snakefile for mers. Will change other snakefiles if works correctly for mers.

---
+++ notebooks/scripts/cluster_mutation.py
@@ -0,0 +1,111 @@
+from augur.io import read_sequences
+from augur.utils import read_node_data
+from collections import OrderedDict
+import pandas as pd
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--reference-alignment", required=True, help="aligned FASTA file with the reference strain")
+    parser.add_argument("--alignment", required=True, help="aligned FASTA file of diseases")
+    parser.add_argument("--metadata", required=True, help="metadata with clade information")
+    parser.add_argument("--metadata-column", default="MCC", help="metadata column to find clade information")
+    parser.add_argument("--output", help="the name of the csv file to be outputted")
+
+    args = parser.parse_args()
+
+    reference = []
+    for sequence in read_sequences(args.reference_alignment):
+        reference.append(str(sequence.seq))
+
+    reference = reference[0]
+
+    sequences_by_name = OrderedDict()
+
+    for sequence in read_sequences(args.alignment):
+        sequences_by_name[sequence.id] = str(sequence.seq)
+
+    sequence_names = list(sequences_by_name.keys())
+
+    # Index cluster name by sequence name (Python dict) from metadata
+    # NOTE TO JOHN: Not sure if json is necessary here since it's the default MCC value given
+    if args.metadata.endswith(".json"):
+        node_data = read_node_data(args.metadata)
+        clade_annotations = pd.DataFrame([
+            {"strain": strain, "clade": annotations[args.metadata_column]}
+            for strain, annotations in node_data["nodes"].items()
+            if strain in sequences_by_name
+        ])
+        clade_annotations = clade_annotations.set_index("strain")
+        clade_annotations = clade_annotations[clade_annotations.clade != "unassigned"]
+    elif args.metadata.endswith(".csv") :
+        clade_annotations = pd.read_csv(args.metadata)
+        clade_annotations.set_index("strain")
+        clade_annotations["clade"] = clade_annotations[args.metadata_column]
+        clade_annotations = clade_annotations[clade_annotations.clade != "-1"]
+        clade_annotations = clade_annotations[["strain", "clade"]]
+
+    # Find mutations per cluster relative to reference as Python dictionary of sets indexed by cluster name
+    clade = clade_annotations.groupby(["clade"])
+    strains_per_cluster_reference = {}
+    for clade in clade_annotations.groupby(["clade"]):
+        strains_per_cluster = set(clade[1].index)
+        strains_per_cluster_reference[clade[0]] = strains_per_cluster
+
+    mutations_per_cluster_reference = {}
+    for clade in strains_per_cluster_reference:
+        mutations=[]
+        for name in strains_per_cluster_reference[clade]:
+            strain = sequences_by_name[name]
+            # compare each strain to the reference
+            for site in range(len(reference)):
+                if strain[site] != reference[site]:
+                    # Report the site in 1-based coordinates.
+                    mutations.append(
+                        {
+                            "cluster": name,
+                            "site": site + 1,
+                            "reference_allele": reference[site],
+                            "alternate_allele": strain[site],
+                        }
+                    )
+
+        mutations = pd.DataFrame(mutations)
+        mutations["mutation"] = mutations.apply(
+            lambda row: row["reference_allele"] + str(row["site"]) + row["alternate_allele"],
+            axis=1,
+        )
+        mutations_per_cluster_reference[clade] = set(mutations["mutation"])
+
+    # print(mutations_per_cluster_reference)
+
+    # Remove mutations that are present in all clusters (reference-only mutations)
+    # Intersection of all sets in sets by cluster name to find shared mutations
+    shared_reference_mutations = mutations_per_cluster_reference[list(mutations_per_cluster_reference)[0]]
+    for mutation in mutations_per_cluster_reference:
+        shared_reference_mutations = shared_reference_mutations.intersection(mutations_per_cluster_reference[mutation])
+        
+    # Remove resulting intersection from each cluster set
+    for mutation in mutations_per_cluster_reference:
+        mutations_per_cluster_reference[mutation] = mutations_per_cluster_reference[mutation] - shared_reference_mutations
+
+    # For each pair of clusters, find mutations that only exist in “from” and “to” clusters
+    # Nested for loop of clusters, excluding self-comparisons
+    mutations = []
+    for mutation_1 in mutations_per_cluster_reference:
+        for mutation_2 in mutations_per_cluster_reference:
+            if (mutation_1 != mutation_2):
+                # Find set difference for from and to (from - to)
+                set_diff = mutations_per_cluster_reference[mutation_1] - mutations_per_cluster_reference[mutation_2]
+                # Report from cluster, to cluster, mutation (e.g., 750G)
+                for item in set_diff:
+                    mutations.append(
+                        { "from": mutation_1,
+                        "to": mutation_2,
+                        "mutation": item
+                        }
+                    )
+    mutations_df = pd.DataFrame(mutations)
+    mutations_df.to_csv(args.output, index=False)              
