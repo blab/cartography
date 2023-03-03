9bbae866a85066303c52510d359d5cbf36983d52
John Huddleston <huddlej@gmail.com>
2023-03-03 15:58:23 -0800

    Use pathogen-specific cross-validation params
    Calculates train/test splits from a continuous range of time
    units (e.g., generations) and defines pathogen-specific cross-validation
    parameters like the number of generations to include in each train/test
    split and the length of the gap between train and test periods.

--- notebooks/scripts/create_time_series_train_test_split.py
+++ notebooks/scripts/create_time_series_train_test_split.py
@@ -16,6 +16,7 @@ if __name__ == '__main__':
     parser.add_argument("--time-column", required=True, help="integer column in the metadata to use for time series subsetting (e.g., generation, year, etc.)")
     parser.add_argument("--total-train-test-splits", type=int, required=True, help="total number of train/test splits to create")
     parser.add_argument("--train-test-split", type=int, required=True, help="index of specific train/test splits to output subset data for")
+    parser.add_argument("--train-test-size", type=int, required=True, help="number of indices to include in train/test splits")
     parser.add_argument("--gap", default=0, type=int, help="gap between train/test time points in units of the given time column")
     parser.add_argument("--output-training-alignment", required=True, help="alignment FASTA for training")
     parser.add_argument("--output-training-genetic-distances", required=True, help="genetic distance matrix for training")
@@ -43,31 +44,38 @@ if __name__ == '__main__':
         for strain, strain_group in metadata.groupby(args.time_column)
     }
 
+    # Find all available time units (e.g., generations) and calculate a range of
+    # continuous values between the minimum and maximum values. These values
+    # will be the input to the time series split logic below.
     time_units = metadata[args.time_column].drop_duplicates().values
+    continuous_time_units = list(range(time_units.min(), time_units.max() + 1))
 
     # Generate time series train/test splits with an equal size for both train
     # and test data.
-    n_samples = time_units.shape[0]
-    n_splits = args.total_train_test_splits
-    max_split_size = int(n_samples / (n_splits + 1))
-
     time_series_cv = TimeSeriesSplit(
-        n_splits=n_splits,
-        max_train_size=max_split_size,
-        test_size=max_split_size,
+        n_splits=args.total_train_test_splits,
+        max_train_size=args.train_test_size,
+        test_size=args.train_test_size,
         gap=args.gap,
     )
 
-    for i, (train_time_index, test_time_index) in enumerate(time_series_cv.split(time_units)):
+    for i, (train_time_index, test_time_index) in enumerate(time_series_cv.split(continuous_time_units)):
         # Only output data for the requested train/test split.
         if i == args.train_test_split:
             strains_train = set()
             for time_index in train_time_index:
-                strains_train.update(strains_by_time[time_units[time_index]])
+                # Not all values in the continous range of time units will be
+                # sampled by the simulation output, so check for whether the
+                # current time index has samples or not.
+                time_unit = continuous_time_units[time_index]
+                if time_unit in strains_by_time:
+                    strains_train.update(strains_by_time[time_unit])
 
             strains_test = set()
             for time_index in test_time_index:
-                strains_test.update(strains_by_time[time_units[time_index]])
+                time_unit = continuous_time_units[time_index]
+                if time_unit in strains_by_time:
+                    strains_test.update(strains_by_time[time_unit])
 
             break
 
