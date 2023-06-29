fe7f2e9e9dfef4fac4d06c452ac76f8b031191e2
John Huddleston <huddlej@gmail.com>
2023-06-29 16:52:55 -0700

    Use intermediate files for clade, date, and y axis
    Instead of using Auspice JSONs that get created at the end of the
    workflow to create a table of metadata that we used during clustering,
    build that table from intermediate files with the same information. This
    change removes circular logic where the Auspice JSON depended on the
    final clustered embedding JSONs which in turn depended on the Auspice
    JSON for metadata.

---
+++ notebooks/scripts/node_data_to_table.py
@@ -0,0 +1,48 @@
+import sys
+sys.path.append("../")
+
+import argparse
+from augur.utils import read_node_data, read_tree
+import pandas as pd
+
+from Helpers import get_y_positions
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tree", required=True, help="Newick tree")
+    parser.add_argument("--node-data", nargs="+", required=True, help="node data JSON(s) to extract attributes from")
+    parser.add_argument("--include-internal-nodes", action="store_true", help="include data from internal nodes in output")
+    parser.add_argument("--attributes", nargs="+", help="names of attributes to export from the given tree")
+    parser.add_argument("--output", required=True, help="tab-delimited file of attributes per node of the given tree")
+
+    args = parser.parse_args()
+
+    # Load tree.
+    tree = read_tree(args.tree)
+
+    # Load node data.
+    node_data = read_node_data(args.node_data)["nodes"]
+
+    # Collect attributes per node from the tree to export.
+    records = []
+
+    heights = get_y_positions(tree)
+    for node in tree.find_clades(terminal=True):
+        if node.is_terminal() or args.include_internal_nodes:
+            record = {
+                "strain": node.name,
+                "y_value": heights[node],
+            }
+
+            for attribute in args.attributes:
+                if attribute in node_data[node.name]:
+                    record[attribute] = node_data[node.name][attribute]
+                else:
+                    print(f"Attribute '{attribute}' missing from node '{node.name}'", file=sys.stderr)
+
+            records.append(record)
+
+   # Convert records to a data frame and save as a tab-delimited file.
+    df = pd.DataFrame(records)
+    df.to_csv(args.output, sep="\t", header=True, index=False, float_format="%.2f")
