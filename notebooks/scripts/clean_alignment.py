bc8b97a0ec48fa2aa2583486919f2e16b7886cd5
John Huddleston <huddlej@gmail.com>
2023-10-10 11:47:34 -0700

    Drop columns with N bases from HA/NA alignments
    Adds a script and Snakemake rule to drop columns with N characters from
    the HA and HA/NA alignments used by PCA (including when PCA is used to
    initialize t-SNE embeddings) and updates rules in the HA/NA workflow to use
    these cleaned alignments. This change does not affect distance-based
    methods, since the distance matrix gets calculated from the original
    alignments. However, the columns with Ns are uninformative for PCA
    except to detect samples with Ns, so this change improves the biological
    value of the PCA embeddings for HA and NA.

---
+++ notebooks/scripts/clean_alignment.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import argparse
+from Bio import AlignIO
+from functools import reduce
+import operator
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--alignment", required=True, help="alignment from which to drop all columns with N characters")
+    parser.add_argument("--output", required=True, help="alignment without N characters")
+    args = parser.parse_args()
+
+    # Load alignment.
+    alignment = AlignIO.read(args.alignment, "fasta")
+
+    # Find positions to drop from the alignment.
+    drop_indexes = [
+        i
+        for i in range(alignment.get_alignment_length())
+        if "N" in alignment[:, i]
+    ]
+
+    # Collect positions that will not be dropped.
+    edited_alignment = reduce(
+        operator.add,
+        (
+            alignment[:, index:index+1]
+            for index in range(alignment.get_alignment_length())
+            if index not in drop_indexes
+        )
+    )
+
+    # Save the new alignment.
+    AlignIO.write(
+        edited_alignment,
+        args.output,
+        "fasta",
+    )
