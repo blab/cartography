4a3aea6d1595032d1d3e6bb6a7311ebaad0e0cd6
John Huddleston <huddlej@gmail.com>
2023-12-19 14:13:42 -0800

    Move supporting information into a separate file
    Creates a separate TeX document to represent figures and tables in the
    supporting information section instead of including this content inline
    in the main text.
    
    Closes #74

--- manuscript/build.sh
+++ manuscript/build.sh
@@ -7,9 +7,13 @@ rm -f *.bbl
 rm -f *.bcf
 rm -f *.blg
 rm -f .DS_Store
-rm -f ${TITLE}.pdf
+rm -f ${TITLE}*.pdf
 
 pdflatex -draftmode ${TITLE}
 bibtex ${TITLE}
 pdflatex -draftmode ${TITLE}
 pdflatex ${TITLE}
+
+pdflatex -draftmode ${TITLE}_supplement
+pdflatex -draftmode ${TITLE}_supplement
+pdflatex ${TITLE}_supplement
