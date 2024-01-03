134f9ca491a28ae7e031fa2bccb190d07aed55ea
John Huddleston <huddlej@gmail.com>
2024-01-03 12:32:14 -0800

    Clarify meaning and layout of accuracy table
    Clarifies that the accuracy table (Table 1) reflects the optimal
    threshold values per pathogen, genetic group type, and embedding method
    and the corresponding VI values for each threshold. Decouples the
    pathogen name (e.g., "Influenza H3N2") from the genetic group used to
    optimize the cluster threshold value (e.g., "Nextstrain clade") by
    adding a separate column for the genetic group to the table.

--- notebooks/scripts/concat_KDE_tables.py
+++ notebooks/scripts/concat_KDE_tables.py
@@ -2,6 +2,7 @@ import argparse
 import pandas as pd
 import numpy as np
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--tables", nargs="+", help="KDE plots")
@@ -9,29 +10,19 @@ if __name__ == "__main__":
     parser.add_argument("--output-table", help="rmarkdown table in file")
     parser.add_argument("--separator", default=',', help="separator between columns in the given tables")
     parser.add_argument("--disease-names", nargs="+", help="names of diseases, separated by space")
+    parser.add_argument("--genetic-group-types", nargs="+", help="names of genetic group types per disease name listed above")
 
     args = parser.parse_args()
 
     # Concatenate tables.
-    df = pd.concat([
-        pd.read_csv(table_file, sep=args.separator)
-        for table_file in args.tables
-    ], ignore_index=True)
+    tables = []
+    for table_file, pathogen, genetic_group_type in zip(args.tables, args.disease_names, args.genetic_group_types):
+        df = pd.read_csv(table_file, sep=args.separator)
+        df["Pathogen"] = [pathogen] + [""] * (df.shape[0] - 1)
+        df["Genetic Group Type"] = [genetic_group_type] + [""] * (df.shape[0] - 1)
+        tables.append(df)
 
-    table_index = []
-    for i in range(0, len(args.tables)):
-        table_index.append(args.disease_names[i])
-        table_index.append([" "] * (len(pd.read_csv(args.tables[i], sep=args.separator).index) -1) )
-
-    def flatten(A):
-        rt = []
-        for i in A:
-            if isinstance(i,list): rt.extend(flatten(i))
-            else: rt.append(i)
-        return rt
-
-    df.index = flatten(table_index)
-    df.index.name = "Pathogen"
+    df = pd.concat(tables, ignore_index=True)
 
     embedding_name_by_abbreviation = {
         "pca": "PCA",
@@ -47,12 +38,12 @@ if __name__ == "__main__":
     })
 
     if args.output_csv:
-        df.to_csv(args.output_csv, sep=args.separator, header=True, index=True)
+        df.to_csv(args.output_csv, sep=args.separator, header=True)
 
     if args.output_table:
         df.reset_index().to_latex(
             args.output_table,
             bold_rows=True,
             index=False,
-            columns=["Pathogen", "Method", "VI", "Threshold"],
+            columns=["Pathogen", "Genetic Group Type", "Method", "VI", "Threshold"],
         )
