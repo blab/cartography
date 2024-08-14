a4ed8cc957f9d8c050bfe5f9c828447bb2a4cd27
John Huddleston <huddlej@gmail.com>
2024-08-14 13:35:23 -0700

    Add results for single clade cluster accuracy
    Adds results tables and figures for single clade cluster analysis including
    rules to filter the lineage/cluster counts to exclude unclustered sequences and
    plot the lineage/cluster counts for the unfiltered table.

---
+++ sars-cov-2-nextstrain/scripts/plot-single-clade-lineage-and-cluster-counts.py
@@ -0,0 +1,28 @@
+import argparse
+import altair as alt
+import pandas as pd
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--counts", required=True, help="path to TSV of counts by lineage and cluster")
+    parser.add_argument("--lineage-column", required=True, help="name of column in counts file for lineages")
+    parser.add_argument("--cluster-column", required=True, help="name of column in counts file for clusters")
+    parser.add_argument("--output", required=True, help="PNG dot plot of counts by lineage and cluster")
+
+    args = parser.parse_args()
+
+    df = pd.read_csv(
+        args.counts,
+        sep="\t",
+    )
+
+    chart = alt.Chart(df).mark_circle().encode(
+        x=alt.X(f"{args.lineage_column}:N", title="Pango lineage"),
+        y=alt.Y("{args.cluster_column}:N", title="Cluster from t-SNE"),
+        size="count:Q",
+    ).properties(
+        width=400,
+        height=400,
+    )
+    chart.save(args.output, format="png", scale_factor=2.0)
